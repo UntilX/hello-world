ad2bd52ad3e59c976b4ac04721df6658be02c5dc
unt <455570682@qq.com>
2022-05-04 15:48:25 +0800

    开发分支添加了两个方法

--- a.js
+++ a.js
@@ -2,7 +2,10 @@
  * @Description: 
  * @Author: xwt
  * @Date: 2022-05-03 23:01:55
- * @LastEditTime: 2022-05-04 14:26:15
+ * @LastEditTime: 2022-05-04 15:47:22
  * @LastEditors: xwt
  */
 xwtdev01
+function a() {
+    console.log('123');
+}
--- b.js
+++ b.js
@@ -2,7 +2,10 @@
  * @Description: 
  * @Author: xwt
  * @Date: 2022-05-03 23:02:30
- * @LastEditTime: 2022-05-04 14:26:22
+ * @LastEditTime: 2022-05-04 15:47:45
  * @LastEditors: xwt
  */
 xwtdev02
+function b() {
+    console.log('123');
+}
