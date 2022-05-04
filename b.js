6337b7a45f4e7f08305c39dc35faf14d63847593
unt <455570682@qq.com>
2022-05-04 19:19:21 +0800

    bendippddev提交d函数

--- b.js
+++ b.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: xwt
  * @Date: 2022-05-03 23:02:30
- * @LastEditTime: 2022-05-04 18:02:41
+ * @LastEditTime: 2022-05-04 19:19:00
  * @LastEditors: xwt
  */
 ppdev02
@@ -10,3 +10,6 @@ function c() {
     console.log("c");
 }
 c();
+function d() {
+    console.log("d");
+}
